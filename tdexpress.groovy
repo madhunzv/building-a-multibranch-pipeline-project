65a9a063d27c9b5453671b043a0d19b564e4d2b1
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 22:00:51 +0530

    debug

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -1,21 +1,23 @@
 def datas
-def var1
+//def var1
 pipeline{
     agent none
     parameters{
         string(name: 'Timestamp', description: 'Timestamp Folder Name to Download OVA')
     }
     stages{
-        stage('Build VMDK'){
+        stage('Build'){
             agent {label 'master'}
             steps{
                 script{
                     def workspace= pwd()
                     datas = readYaml (file:"${workspace}/config.yml")
-                    var1 = datas.config1.toString()
+                    def var1 = datas.config1.toString()
                     def var2 = datas.config2.toString()
                     //echo props.config1.toString()
+                    echo var1
                 }
+                
                 echo datas.config1.toString()
                 sh "chmod +x build.sh && ./build.sh ${params.Timestamp} ${var1}"
                 //echo "Timestamp is ${params.Timestamp}"
