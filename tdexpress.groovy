5f4d42d36b5fd128348b3c1f8154dd9f669cb69a
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 17:56:48 +0530

    fixed

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -10,7 +10,7 @@ pipeline{
                 script{
                     def workspace= pwd()
                     datas = readYaml (file:"${workspace}/config.yml")
-                    def var = props.skipconfig.config1.toString()
+                    //def var = props.skipconfig.config1.toString()
                     //echo props.config1.toString()
                 }
                 echo datas.config1.toString()
