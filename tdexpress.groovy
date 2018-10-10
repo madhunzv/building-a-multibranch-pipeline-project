67b73ce51ab820bdd688a1982c29641cc20b2be2
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 11:24:53 +0530

    changes

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -2,11 +2,12 @@ pipeline{
     agent none
     def workspace = pwd()
     def props = readYaml file: "${workspace}/config.yml"
+    def x = props['skipconfig'][0]['config1']
     stages{
         stage('Build VMX'){
             agent {label 'vmx_build'}
             steps{
-                echo $
+                echo $x
             }
         }
     }
