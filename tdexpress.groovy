f1d85dc9eeff3980843cb7a17c663082b17e820e
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 17:46:53 +0530

    added node config

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -1,14 +1,18 @@
+node {
+    def workspace= pwd()
+    def props = readYaml (file:"${workspace}/config.yml")
+}
 pipeline{
     agent none
 
     stages{
-        stage('Build VMX'){
+        stage('Build VMDK'){
             agent {label 'master'}
             steps{
                 
                 script{
-                    def workspace= pwd()
-                    def props = readYaml (file:"${workspace}/config.yml")
+                    //def workspace= pwd()
+                    //def props = readYaml (file:"${workspace}/config.yml")
                     def var = props.skipconfig.config1.toString()
                     //echo props.config1.toString()
                 }
