0bd1d05fe1c2baa9ce77ecd26a8e83efe512f124
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 22:27:53 +0530

    fixed issues

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -1,9 +1,7 @@
 import groovy.transform.Field
-@Field
-def datas
-@Field
-def var1
-//def var1
+@Field def datas
+@Field def var1
+@Field def var2
 pipeline{
     agent none
     parameters{
@@ -17,15 +15,12 @@ pipeline{
                     def workspace= pwd()
                     datas = readYaml (file:"${workspace}/config.yml")
                     var1 = datas.config1.toString()
-                    def var2 = datas.config2.toString()
+                    var2 = datas.config2.toString()
                     //echo props.config1.toString()
                 }
                 
                 echo datas.config1.toString()
-                sh "chmod +x build.sh && ./build.sh ${params.Timestamp} ${var1}"
-                //echo "Timestamp is ${params.Timestamp}"
-                //print "${workspace}"
-                //print "${var}"
+                sh "chmod +x build.sh && ./build.sh ${params.Timestamp} ${var1} ${var2}"
             }
         }
     }
