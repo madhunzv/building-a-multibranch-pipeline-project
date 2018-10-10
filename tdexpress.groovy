1b146eaf518bfe54a179a33c416f0ee602014bd4
Veluguri <madhu.veluguri@teradata.com>
2018-10-10 11:52:33 +0530

    vars added to stage

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -1,10 +1,10 @@
 pipeline{
     agent none
     stages{
-        def workspace = pwd()
-        def props = readYaml file: "${workspace}/config.yml"
-        def x = props['skipconfig'][0]['config1']
         stage('Build VMX'){
+            def workspace = pwd()
+            def props = readYaml file: "${workspace}/config.yml"
+            def x = props['skipconfig'][0]['config1']
             agent {label 'vmx_build'}
             steps{
                 echo $x
