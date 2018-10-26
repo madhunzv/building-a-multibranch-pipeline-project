82437dd8a5ccaef03ede882f566bbff1000e85db
madhunzv <madhusudhanveluguri@gmail.com>
2018-10-26 11:22:49 +0530

    Update tdexpress.groovy

--- tdexpress.groovy
+++ tdexpress.groovy
@@ -9,7 +9,7 @@ pipeline{
     }
     stages{
         stage('Build'){
-            agent {label 'master'}
+            //agent {label 'master'}
             steps{
                 script{
                     workspace= pwd()
