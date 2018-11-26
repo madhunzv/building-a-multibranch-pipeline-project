556fba5d8778fab002587869c75790be672e5d44
sd185026 <Srikanth.Doosa@Teradata.com>
2018-11-26 11:51:07 +0530

    kldjf

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIMESTAMP=$1
-VAR1=$2
-VAR2=$3
+#VAR1=$2
+#VAR2=$3
 echo "Timestamp value is $TIMESTAMP"
 echo "$VAR1 and $VAR2 from pipeline"
 #echo "$VAR1 from pipeline"
--- tdexpress.groovy
+++ tdexpress.groovy
@@ -11,16 +11,28 @@ pipeline{
         stage('Build'){
             agent {label 'master'}
             steps{
+                sh 'sed -ri "s/(.*teradata\/)(.*)/\1${params.Timestamp}\/\2/g" /root/tdexpress_build/try.txt'
                 script{
                     workspace= pwd()
                     datas = readYaml (file:"${workspace}/config.yml")
                     var1 = datas.config1.toString()
                     var2 = datas.config2.toString()
-                    //echo props.config1.toString()
                 }
                 
                 echo datas.config1.toString()
-                sh "chmod +x build.sh && ./build.sh ${params.Timestamp} ${var1} ${var2}"
+                sh "chmod +x build.sh && ./build.sh ${params.Timestamp}"
+            }
+        }
+        stage('Load VM'){
+            agent {label 'vmx_player'}
+            steps{
+                dir("C:\\Users\\admin\\Documents\\VIX_Config"){
+                    bat "./download_tde.bat ${params.Timestamp}"
+                }
+                script{
+                    props = readYaml (file:"C:\\TD-Express\\props.yml")
+                    
+                }
             }
         }
     }
